ab5e3e6fe3cfdae3c04e1c4e77f54632a3097b75
oali <oaliarshad@gmail.com>
2021-09-03 19:57:27 -0700

    made turtle move in a square

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 screen = Screen()
+timmy.color("green")
+i = 0
+while i < 4:
+    timmy.forward(50)
+    timmy.right(90)
+    i += 1
 screen.exitonclick()
-# test commit
